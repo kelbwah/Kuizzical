f20a8247d0be7ad2be7722f9a6e8dc6e31c888b6
kelbwah <kbebops@gmail.com>
2024-03-21 14:01:59 -0700

    s3 again again

--- api/controllers/s3-controller.js
+++ api/controllers/s3-controller.js
@@ -2,9 +2,9 @@ const logic = require('../logic');
 
 const s3Upload = async (req, res) => { // Uploading specific s3 object
     try {
-        return res.status(200).json({ imageUrl: req.body.imageUrl }); 
+        return res.status(201).json({ imageUrl: req.body.imageUrl }); 
     } catch (err) {
-        return res.status(500).json({ error: err });
+        return res.status(500).json({ gotteem: `your body: ${JSON.stringify(req.body)}`, error: err });
     };
 };
 
--- client/src/services/S3ServiceWrapper.js
+++ client/src/services/S3ServiceWrapper.js
@@ -5,11 +5,15 @@ export const uploadImage = async (image) => {
     const formData = new FormData();
     formData.append('image', image);
 
-    console.log(JSON.stringify(formData));
+    console.log('Stringified data: ', JSON.stringify(formData));
 
-    const data = await axios.post('/s3/upload/image', formData);
+    const data = await axios.post('/s3/upload/image', formData, {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
 
-    console.log(data);
+    console.log('Data: ', data);
 
     return data;
 };
