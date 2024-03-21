db81d39079f3c20917c0f2591b485b464176e21c
kelbwah <kbebops@gmail.com>
2024-03-21 12:55:37 -0700

    updated client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,6 @@ import SceneTemplate from './scenes/SceneTemplate.jsx';
 
 function App() {
     axios.defaults.baseURL = 'https://kuizzical-prod-exhru3sgq-kelbwah.vercel.app/api';
-    axios.defaults.withCredentials = true;
 
     return (
         <BrowserRouter basename='/'>
