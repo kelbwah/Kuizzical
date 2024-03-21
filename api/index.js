0b0e8a9fc605fc6e6cb5e894bc5f7229324a1ee0
kelbwah <kbebops@gmail.com>
2024-03-21 13:00:18 -0700

    cors

--- api/index.js
+++ api/index.js
@@ -13,7 +13,9 @@ dotenv.config();
 const app = express();
 
 // Setting up some CORS and JSON stuff for express
-app.use(cors());
+app.use(cors({
+    origin: 'https://kuizzical.vercel.app', 
+}));
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({
