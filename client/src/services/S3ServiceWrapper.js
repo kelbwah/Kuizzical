36e50daa535edd166efd7e77f1619ed1b10ad3a9
kelbwah <kbebops@gmail.com>
2024-03-21 13:56:43 -0700

    s3 again

--- client/src/services/S3ServiceWrapper.js
+++ client/src/services/S3ServiceWrapper.js
@@ -5,9 +5,11 @@ export const uploadImage = async (image) => {
     const formData = new FormData();
     formData.append('image', image);
 
+    console.log(JSON.stringify(formData));
+
     const data = await axios.post('/s3/upload/image', formData);
+
+    console.log(data);
 
     return data;
 };
-
-export const deleteImage = (image) => {};
